3e29b08d5430a3f82bacf3ee5b8579de6bbde9f6
Tobias Bradtke <webwurst@gmail.com>
2017-08-15 02:33:53 +0200

    testing

--- server.py
+++ server.py
@@ -1,19 +1,14 @@
 from sanic import Sanic
 from sanic.response import json
-# import sanic.response
-# sanic.request.Request
-
 import logging
 
 app = Sanic()
 
 @app.route("/")
 async def test(request):
-    print(request.headers)
-    logging.warning(request.headers)
-
-    return json({"hello": "world"})
+    # logging.warning(request.headers)
+    user_name = request.headers['token-claim-sub']
+    return json({"hello": user_name})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
-    # app.run(log_config=LOGGING)
