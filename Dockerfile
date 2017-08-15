bb186516fef4523f4fc5e8cd4f14ed5e6b19c6e0
Tobias Bradtke <webwurst@gmail.com>
2017-08-15 02:04:33 +0200

    testing

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM python:3.6-alpine3.6
 RUN apk --no-cache add tini \
   && apk --no-cache add --virtual .build-deps \
     make gcc python-dev musl-dev \
-  && pip install \
+  && pip install --no-cache-dir \
     "Sanic~=0.6.0" \
   && apk del .build-deps
 
--- server.py
+++ server.py
@@ -1,11 +1,19 @@
 from sanic import Sanic
 from sanic.response import json
+# import sanic.response
+# sanic.request.Request
+
+import logging
 
 app = Sanic()
 
 @app.route("/")
 async def test(request):
+    print(request.headers)
+    logging.warning(request.headers)
+
     return json({"hello": "world"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
+    # app.run(log_config=LOGGING)
